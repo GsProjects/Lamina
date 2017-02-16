0f780d49fed6d8b7119363bf4160691de8ed6895
GsProjects <gearoidlacey@gmail.com>
2017-02-16 14:59:53 +0000

    Latest code for analyse paths functionality

--- www/analyse_paths.py
+++ www/analyse_paths.py
@@ -21,7 +21,7 @@ def analyse(user):
 def get_current_location(userID:str):
     cnx2 = create_connection()
     cursor = cnx2.cursor()
-    query = ("Select longitude,latitude from currentCoordinates where username = %s")
+    query = ("Select currentCoordinates.longitude,currentCoordinates.latitude,Animal.animalIdentifier from currentCoordinates INNER JOIN Animal ON currentCoordinates.trackingID=Animal.trackingID where username = %s")
     cursor.execute(query,(userID, ))
     result = cursor.fetchall()
     cursor.close()
